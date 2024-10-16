7dfae64119a4e6c3bd836148674c1f82ad5f0ae3
yellowhat <yellowhat46@gmail.com>
2024-10-16 17:47:02 +0000

    Allow to set PORT

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+PORT=${PORT:-8080}
+
 export SEARX_VERSION=$(python3 -c "import six; import searx.version; six.print_(searx.version.VERSION_STRING)" 2>/dev/null)
-printf '[INFO] Searx version: %s\n\n' "${SEARX_VERSION}"
+echo "[INFO] Searx version: ${SEARX_VERSION}"
 
 echo "[INFO] Change ultrasecretkey"
 sed -e "s/ultrasecretkey/$(openssl rand -hex 32)/g" \
     -i /etc/searxng/settings.yml
 
-# Start uwsgi
-uwsgi --master --http-socket "0.0.0.0:8080" "uwsgi.ini"
+echo "[INFO] Starting uwsgi on port: $PORT"
+uwsgi --master --http-socket "0.0.0.0:$PORT" "uwsgi.ini"
