80f8d46c47cc2e600c762691d512e1d4942a0c2b
yellowhat <yellowhat46@gmail.com>
2024-10-30 14:28:30 +0000

    Add /metrics (#44)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
+# Startup script
 
 PORT=${PORT:-8080}
-
-export SEARX_VERSION=$(python3 -c "import six; import searx.version; six.print_(searx.version.VERSION_STRING)" 2>/dev/null)
-echo "[INFO] Searx version: ${SEARX_VERSION}"
 
 echo "[INFO] Change ultrasecretkey"
 sed -e "s/ultrasecretkey/$(openssl rand -hex 32)/g" \
