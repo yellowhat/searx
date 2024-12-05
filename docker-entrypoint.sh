f77f3026ce972d35955b880bba162aa934856d7a
yellowhat <yellowhat46@gmail.com>
2024-12-05 21:33:13 +0000

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,11 @@
 
 PORT=${PORT:-8080}
 
+echo "[INFO] Copy settings.yaml to /tmp"
+cp "$SEARXNG_SETTINGS_PATH" /tmp/settings.yaml
+
+export SEARXNG_SETTINGS_PATH=/tmp/settings.yaml
+
 echo "[INFO] Change ultrasecretkey in $SEARXNG_SETTINGS_PATH"
 sed -e "s/ultrasecretkey/$(hexdump -n 32 -e '32/1 "%02x"' /dev/urandom)/g" \
     -i "$SEARXNG_SETTINGS_PATH"
